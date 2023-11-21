c9f9fa1f3a7aad91e39e7832308a6d3254deb092
Emanoel-ferro <ferroemanoel1@gmail.com>
2023-11-21 15:54:53 -0300

    tcc

---
+++ teste002/javascript/noticias.js
@@ -0,0 +1,7 @@
+//link externo das noticias
+
+let noticias = document.querySelector('.1');
+
+noticias.addEventListener('click', ()=>{
+    window.location.href="https://epbr.com.br/amazon-anuncia-primeiro-parque-eolico-no-brasil/"
+})
